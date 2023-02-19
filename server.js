35991abd72dbbf2d59c24a370741e3cbe414d21e
nabilainas <ainaspro@gmail.com>
2023-02-19 20:37:34 +0100

    Add user schema

--- server.js
+++ server.js
@@ -1,58 +1,16 @@
 const express = require("express")
-const mongoose = require("mongoose")  
+const mongoose = require("./database")  
+const User = require("./User")
 const pkg = require("./package.json")
 const port = 8080
 
-mongoose.connect("mongodb://localhost:27017/rest-api", { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Connexion à MongoDB réussie"))
-  .catch(() => console.log("Connexion à MongoDB échouée"))
-
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  firstname: {
-    type: String,
-    required: true,
-  },
-  lastname: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    required: true,
-  }
-})
-const User = mongoose.model("User", userSchema)
-
-
-const user = new User({
-  firstname: "JohnDoe",
-  lastname: "Doe",
-  role: "admin",
-  email: "johndoe@example.com",
-  password: "password123"
-})
-
-user.save()
-  .then(() => console.log("Utilisateur créé avec succès"))
-  .catch(() => console.log("Erreur lors de la création de l'utilisateur"))
-
 const server = express()
 
-server.get("/", (req, res) => {
-  return res.status(200).json({
-    name : pkg.name,
-    author : pkg.author,
-    version : pkg.version,
-    description : pkg.description,
-  })  
+
+server.use(express.json())
+
+mongoose.connection.on("disconnect", e => {
+  console.log(e)
 })
 
 server.get("/users", async (req, res) => {
@@ -62,6 +20,30 @@ server.get("/users", async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: error.message })
   }
+})
+server.post("/users", async (req, res) => {
+  const user = new User({
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    role: req.body.role,
+    email: req.body.email,
+    password: req.body.password,
+  })
+  try {
+    const newUser = await user.create(req.body)
+    res.status(201).json(newUser, "Utilisateur créé avec succès")
+  } catch (error) {
+    res.status(500).json({ message: "Erreur lors de la création de l'utilisateur" })
+  }
+})
+
+server.get("/", (req, res) => {
+  return res.status(200).json({
+    name : pkg.name,
+    author : pkg.author,
+    version : pkg.version,
+    description : pkg.description,
+  })  
 })
 
 server.listen(port, (err) => {
