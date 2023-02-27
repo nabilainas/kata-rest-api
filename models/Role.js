1218eb68634dab537fd2dc79e8234b42c243c1f4
nabilainas <ainaspro@gmail.com>
2023-02-27 18:34:53 +0100

    Adding Role model

---
+++ models/Role.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+
+const roleSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  permissions: {
+    create: {
+      type: Boolean,
+      default: false,
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
+    update: {
+      type: Boolean,
+      default: false,
+    },
+    delete: {
+      type: Boolean,
+      default: false,
+    }
+  }
+})
+
+module.exports = mongoose.model("Roles", roleSchema)
